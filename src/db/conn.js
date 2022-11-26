33b3adbb85f7376f8858bbb935e06491930023c4
Furqan Rasool <100808717+yahyalogics@users.noreply.github.com>
2022-11-27 03:02:47 +0500

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
+var dbURI = 'mongodb://localhost:27017/Paiyaa';
 
-mongoose.connect("mongodb://localhost:27017/Paiyaa" , {
+if (process.env.NODE_ENV === 'production') {
+    dbURI = process.env.MONGOLAB_URI;
+}
+mongoose.connect(dbURI , {
     //useCreateIndex:true,
     useNewUrlParser:true,
     useUnifiedTopology:true
