eee3203792bf726eecea4cc7e61c658a7a81b8e2
Furqan Rasool <100808717+yahyalogics@users.noreply.github.com>
2022-11-27 03:30:38 +0500

    online mongo

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const router = require("./router/user");
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
+
 app.use(router);
 app.listen(port , ()=> {
     console.log('API is Live on port' , port);
