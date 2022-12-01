5063b159b797ba809ab90264c0516704a1e710d0
Furqan Rasool <100808717+yahyalogics@users.noreply.github.com>
2022-12-01 17:27:03 +0500

    Update users.js

--- src/models/users.js
+++ src/models/users.js
@@ -41,8 +41,7 @@ const userSchema = new mongoose.Schema({
         } ,
         img:
         {
-            data: Buffer,
-            contentType: String
+            type:String ,
         },
 
 })
