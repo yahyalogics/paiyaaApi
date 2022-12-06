ade46eaf34f41aa5bee93449d598b41010b3e721
Furqan Rasool <100808717+yahyalogics@users.noreply.github.com>
2022-12-06 15:41:13 +0500

    Update user.js

--- src/router/user.js
+++ src/router/user.js
@@ -1,22 +1,44 @@
 const express = require("express");
 require("../db/conn");
 const router = new express.Router();
-const userList = require("../models/users");
+const User = require("../models/users");
 router.use(express.json());
+
+const multer = require("multer");
+const userList = require("../models/users");
+const storage = multer.diskStorage({
+  destination: (req, file, callBack) => {
+    callBack(null, "public/images/");
+  },
+  filename: (req, file, callBack) => {
+    callBack(null, `${Date.now() + file.originalname.split(" ").join("-")}`);
+  },
+});
+let upload = multer({ storage });
 //we will handle post
 
-router.post("/users" ,async(req , res)=>{
-    try{
-        const addinguser = new userList(req.body)
-        console.log(req.body);
-        const insertuser = await addinguser.save();
-        res.status(201).send(insertuser);
+//router.post("/users" ,async(req , res)=>{
+  //  try{
+    //    const addinguser = new User(req.body)
+      //  console.log(req.body);
+        //const insertuser = await addinguser.save();
+        //res.status(201).send(insertuser);
 
-    }catch(e){
-        res.status(400).send(e);
+  //  }catch(e){
+//        res.status(400).send(e);
 
-    }
-})
+    //}
+//})
+
+
+router.post("/users", upload.single("image"), async function (req, res, next) {
+    let user = new userList(req.body);
+    console.log(req.body);
+    if (req.file) user.image = req.file.filename;
+    await user.save();
+    console.log(user);
+    res.send(user);
+  });
 //handling get request
 router.get("/users" , async(req , res)=>{
     try{
