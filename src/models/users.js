ff54fd4f10d7f9d121d518882284a6a972848f3f
Furqan Rasool <100808717+yahyalogics@users.noreply.github.com>
2022-12-06 16:06:09 +0500

    reverse

--- src/models/users.js
+++ src/models/users.js
@@ -1,46 +1,49 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const mongoose  = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     _id:{
-        type : String,
-        required : true,
-    },
-    user: {
-        type: String,
-        required: true,
-    },
-    fname: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    lname: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    phone: {
-        type: Number,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        index: { unique: true },
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    address: {
-        type: Array,
-    },
-    image: {
-        type: String,
-    },
-});
+        type:String,
+        required:true,
+
+    } ,
+    user:{
+            type:String,
+            required:true,
+
+        } ,
+        fname:{
+            type:String ,
+            required:true,
+            trim:true
+        } ,
+        lname:{
+            type:String ,
+            required:true,
+            trim:true
+        } ,
+        phone:{
+            type:Number ,
+            required:true
+        } ,
+        email:{
+            type:String ,
+            required:true ,
+            index : {unique : true},
+        } ,
+        password:{
+            type:String ,
+            required:true,
+            trim:true
+        } ,
+        address:{
+            type:Array ,
+        } ,
+        image:{
+            type:String,
+        }
+
+})
 //we are creating a new collection
-const User = new mongoose.model("User", userSchema);
-module.exports = User;
+const userList = new mongoose.model("userList" , userSchema);
+module.exports = userList;
--- src/router/user.js
+++ src/router/user.js
@@ -1,24 +1,13 @@
 const express = require("express");
 require("../db/conn");
 const router = new express.Router();
-const User = require("../models/users");
+const userList = require("../models/users");
 router.use(express.json());
-
-const multer = require("multer");
-const storage = multer.diskStorage({
-  destination: (req, file, callBack) => {
-    callBack(null, "public/images/");
-  },
-  filename: (req, file, callBack) => {
-    callBack(null, `${Date.now() + file.originalname.split(" ").join("-")}`);
-  },
-});
-let upload = multer({ storage });
 //we will handle post
 
 router.post("/users" ,async(req , res)=>{
     try{
-        const addinguser = new User(req.body)
+        const addinguser = new userList(req.body)
         console.log(req.body);
         const insertuser = await addinguser.save();
         res.status(201).send(insertuser);
@@ -28,22 +17,11 @@ router.post("/users" ,async(req , res)=>{
 
     }
 })
-
-
-router.post("/uploadUser", upload.single("image"), async function (req, res, next) {
-    let user = new User(req.body);
-    console.log(req.body);
-    if (req.file) user.image = req.file.filename;
-    await user.save();
-    console.log(user);
-    res.send(user);
-  });
-
 //handling get request
 router.get("/users" , async(req , res)=>{
     try{
 
-      const getusers = await User.find({});
+      const getusers = await userList.find({});
 
        res.send(getusers);
     }catch(e){
@@ -55,7 +33,7 @@ router.get("/users" , async(req , res)=>{
 router.get("/users/:id" , async(req , res)=>{
     try{
         const _id = req.params.id;
-      const getuser = await User.findById({_id : _id});
+      const getuser = await userList.findById({_id : _id});
        res.send(getuser);
     }catch(e){
         res.status(400).send(e);
@@ -66,7 +44,7 @@ router.get("/users/:id" , async(req , res)=>{
 router.patch("/users/:id" , async(req , res)=>{
     try{
         const _id = req.params.id;
-      const getuser = await User.findByIdAndUpdate(_id , req.body , {
+      const getuser = await userList.findByIdAndUpdate(_id , req.body , {
         new : true
       });
        res.send(getuser);
@@ -80,7 +58,7 @@ router.patch("/users/:id" , async(req , res)=>{
 router.delete("/users/:id" , async(req , res)=>{
     try{
 
-      const getuser = await User.findByIdAndDelete(req.params.id);
+      const getuser = await userList.findByIdAndDelete(req.params.id);
        res.send(getuser);
     }catch(e){
         res.status(500).send(e);
