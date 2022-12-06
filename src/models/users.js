06ed713847a4fcfbbb8bf02a87b2a2c0548f2f0a
Furqan Rasool <100808717+yahyalogics@users.noreply.github.com>
2022-12-06 16:24:36 +0500

    Update users.js

--- src/models/users.js
+++ src/models/users.js
@@ -2,6 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+    _id:{
+        type:String,
+
+    }
     user: {
         type: String,
         required: true,
