dd7330ca99235fd6b11deb1c6fb6e240ae499928
Furqan Rasool <100808717+yahyalogics@users.noreply.github.com>
2022-12-18 12:38:19 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const multer = require('multer');
 require("./db/conn");
 //const userList = require('../src/models/users');
 const router = require("./router/user");
+const cors = require('cors')
 const app = express();
+app.use(cors())
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
@@ -11,10 +13,7 @@ app.use(router);
 app.listen(port , ()=> {
     console.log('API is Live on port' , port);
 })
-const cors = require('cors');
-app.use(cors({
-    origin: 'http://localhost:3000/', // use your actual domain name (or localhost), using * is not recommended
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-    credentials: true
-}))
+
+
+
+
