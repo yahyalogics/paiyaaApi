1033a0cde49f3ed47c2bd830f6cf3c97a4fe17ad
Furqan Rasool <100808717+yahyalogics@users.noreply.github.com>
2022-12-18 12:29:31 +0500

    cors policy update

--- src/app.js
+++ src/app.js
@@ -11,4 +11,10 @@ app.use(router);
 app.listen(port , ()=> {
     console.log('API is Live on port' , port);
 })
-// Step 5 - set up multer for storing uploaded files
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost:3000/', // use your actual domain name (or localhost), using * is not recommended
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+    credentials: true
+}))
